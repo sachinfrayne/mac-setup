4a8278a4e86e00be0d0d67d70f92a6c6e75b9a11
sachin-frayne <sachin.frayne@gmail.com>
2023-12-01 13:55:00 +0100

    setup folders correctly

--- bin/espanso.sh
+++ bin/espanso.sh
@@ -2,6 +2,8 @@
 
 brew_install_cask espanso
 
+mkdir -p ${HOME}/Library/Application\ Support/espanso/match/packages/
+
 cp -f ${HOME}/Source/mac-setup/bin/espanso/*.yml ${HOME}/Library/Application\ Support/espanso/match/packages/
 
 tee ${HOME}/Library/Application\ Support/espanso/match/base.yml <<-'EOF' >/dev/null
