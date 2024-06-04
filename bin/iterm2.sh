c10f5227690dbc9114935da66849dc34d7673862
sachin-frayne <sachin.frayne@gmail.com>
2024-06-04 11:39:42 +0200

    move nerd font defaults to iterm2.sh

--- bin/iterm2.sh
+++ bin/iterm2.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 brew_install_cask iterm2
+
+defaults write com.googlecode.iterm2 "Normal Font" -string "Hack Nerd Font"
+defaults write com.googlecode.iterm2 "Non Ascii Font" -string "Hack Nerd Font"
+
+ITERM2_PREFS="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+EXPORT_PATH="$HOME/Desktop/iterm2_profile_export.plist"
+
+if [ ! -f "$ITERM2_PREFS" ]; then
+  echo "iTerm2 preferences file not found at $ITERM2_PREFS"
+  exit 1
+fi
--- bin/nerd_fonts.sh
+++ bin/nerd_fonts.sh
@@ -2,6 +2,3 @@
 
 brew tap homebrew/cask-fonts
 brew_install_cask font-hack-nerd-font
-
-defaults write com.googlecode.iterm2 "Normal Font" -string "Hack Nerd Font"
-defaults write com.googlecode.iterm2 "Non Ascii Font" -string "Hack Nerd Font"
