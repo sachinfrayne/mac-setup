3177c94688f8804a06be63a54dbd5c9627622310
sachinfrayne <sachin.frayne@gmail.com>
2025-12-29 10:18:29 +0100

    change github username to sachinfrayne

--- bin/git.sh
+++ bin/git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git config --global user.email "sachin.frayne@gmail.com"
-git config --global user.name "sachin-frayne"
+git config --global user.name "sachinfrayne"
 git config --global credential.helper osxkeychain
 git config --global pull.rebase true
